ecc90badb5ce4b52accdd2e817c13bbded1ce883
ManikumarJ <manikumarj6@gmail.com>
2026-02-19 03:42:46 +0530

    updated

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/user",userRoute )
+
+app.listen(PORT, ()=>{
+console.log(`Server is ON : http://localhost:${PORT} `);
+
+})
